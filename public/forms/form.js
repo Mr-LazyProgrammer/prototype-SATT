3080952f4d0073ab653ab475fc99655bedb8e70c
Smruti Ranjan Badatya <35039502+Mr-LazyProgrammer@users.noreply.github.com>
2021-11-15 23:00:15 +0530

    Form config

--- public/forms/form.js
+++ public/forms/form.js
@@ -1,15 +1,24 @@
- // Your web app's Firebase configuration
- const firebaseConfig = {
-    apiKey: "AIzaSyA7RuXVlV37Xn4Cz0Dl0MV6nhwoFOFrLjI",
-    authDomain: "satt-8f551.firebaseapp.com",
-    projectId: "satt-8f551",
-    storageBucket: "satt-8f551.appspot.com",
-    messagingSenderId: "625407997658",
-    appId: "1:625407997658:web:7152024201b999dd3ed7e3",
-    measurementId: "G-VB0R5B025W"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA7RuXVlV37Xn4Cz0Dl0MV6nhwoFOFrLjI",
+  authDomain: "satt-8f551.firebaseapp.com",
+  projectId: "satt-8f551",
+  storageBucket: "satt-8f551.appspot.com",
+  messagingSenderId: "625407997658",
+  appId: "1:625407997658:web:7152024201b999dd3ed7e3",
+  measurementId: "G-VB0R5B025W"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 let contactInfo = firebase.database().ref('submissions');//reference to contact info collection
 
@@ -24,7 +33,7 @@ function submitForm(e){
     const subject = document.querySelector('#subject').value;
     const message = document.querySelector('#message').value;
    
-    saveContactInfo(name, email,subject , message);
+    saveContactInfo(name, email, subject, message);
     document.querySelector('#contactForm').reset();
 }
 
