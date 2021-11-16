d411f5077972fce75f6fdf7721df36294ede63c3
Smruti Ranjan Badatya <35039502+Mr-LazyProgrammer@users.noreply.github.com>
2021-11-16 19:52:31 +0530

    Added Form Config

--- public/forms/form.js
+++ public/forms/form.js
@@ -5,36 +5,43 @@ const firebaseConfig = {
     projectId: "satt-8f551",
     storageBucket: "satt-8f551.appspot.com",
     messagingSenderId: "625407997658",
-    appId: "1:625407997658:web:7152024201b999dd3ed7e3",
-    measurementId: "G-VB0R5B025W"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+    appId: "1:625407997658:web:319542fbf70389413ed7e3",
+    measurementId: "G-2CJRB45840"
+};
 
-let contactInfo = firebase.database().ref('submissions');//reference to contact info collection
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
 
-document.querySelector('#contactForm').addEventListener('submit',
-submitForm);
+let firestore = firebase.firestore()
+const db = firestore.collection("submissions")
+
+let submit = document.getElementById('submit')
+
+submit.addEventListener('click', submitForm)
 
 function submitForm(e){
-    e.preventDefault();
+    e.preventDefault()
     
-    const name = document.querySelector('#name').value;
-    const email = document.querySelector('#email').value;
-    const subject = document.querySelector('#subject').value;
-    const message = document.querySelector('#message').value;
-   
-    saveContactInfo(name, email, subject, message);
-    document.querySelector('#contactForm').reset();
-}
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const subject = document.querySelector('#subject').value
+    const message = document.querySelector('#message').value
 
-function saveContactInfo(name, email, subject, message){
-    let newContactInfo = contactInfo.push();
-
-    newContactInfo.set({
+    db.doc().set({
         Name: name,
         eMail: email,
         Subject: subject,
         Message: message
-    });
+    }).then( ()=>{
+        console.log("Data Saved")
+    }).catch((error) => {
+        console.log(error)
+    })
+   
+    document.querySelector('#contactForm').reset()
+
+    alert("Your Form has been successfully submitted !")
 }
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
